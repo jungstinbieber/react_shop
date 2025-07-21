eeef6d28fa54929e586e14b60e565759ec93fec7
jungstinbieber <>
2025-07-21 14:37:38 +0900

    0721 상품목록구현완료

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,37 @@
 import Card from "../components/Card"
 import bg from "../bg.jpg"
+import { useState } from "react";
 
-function MainPage({fruit}) {
-  return(
+function MainPage({ fruit }) {
+  const [fruitCount,setFruitCount]= useState(3);
+
+  const visibleFruit = fruit.slice(0, fruitCount)
+
+  return (
     <>
-      <div className="main-bg" style={{backgroundImage:'url(' + bg + ')'}}></div>
+      <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')', height:'300px' }}></div>
 
       <div className="container">
         <div className="row">
           {
-            fruit.map((data,i)=>{
-              return(
-                
-                <Card data={data} key={i}/> 
+            fruit.map((data, i) => {
+              return (
+                <Card data={data} key={i} />
               )
             })
           }
         </div>
       </div>
+      {
+        fruitCount > fruit.length? 
+        <div className="alert alert-dabger">더이상 상품이 없습니다</div>
+        :
+      
+
+      <button onCanPlay={()=>{
+        setFruitCount(fruitCount+3)
+      }}>3 개 더보기</button>
+      }
     </>
   )
 }
---
+++ src/pages/Test.jsx
@@ -0,0 +1,15 @@
+function Test({fruit, num}) {
+  // const fruit = props.fruit;
+  // const num = props.num;
+
+  // const {fruit, num} = props;
+
+  console.log(fruit);
+  console.log(num)
+
+  return (
+    <></>
+  )
+}
+
+export default Test
